690eba22ea0b46e73216b84b78f7172bbd4ee849
Geert <geert_dircks@live.nl>
2018-09-27 21:50:27 +0200

    Send to branche exercise 5

---
+++ src/Airplanes/JetEngine.java
@@ -0,0 +1,6 @@
+package Airplanes;
+
+public interface JetEngine {
+    public boolean hasJetEngine();
+    public void startJetEngine();
+}
---
+++ src/Airplanes/PropellorEngine.java
@@ -0,0 +1,6 @@
+package Airplanes;
+
+public interface PropellorEngine {
+    public boolean hasPropellorEngine();
+    public void startPropellorEngine();
+}
